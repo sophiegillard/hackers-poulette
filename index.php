96e56ec2af3c7d6b12df57c2145bde337df59c5d
Sophie Gillard <sophie.x.gillard@gmail.com>
2022-09-14 10:01:03 +0200

    form created and DB

--- index.php
+++ index.php
@@ -1,1 +1,41 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Claim Form</title>
+    </head>
+    <body>
+        <h2>Reclamation form</h2>
+        <form>
+
+            <div>
+                <label for="name">First name</label>
+                <input type="text" name="firstname" value="">
+            </div>
+
+            <div>
+                <label for="lastname">Last name</label>
+                <input type="text" name="lastname" value="">
+            </div>
+
+            <div>
+                <label for="email">Email</label>
+                <input type="email" name="email" value="">
+            </div>
+
+            <div>
+                <label for="file">File</label>
+                <input type="file" name="file" value="">
+            </div>
+
+            <div>
+                <label for="description">Description</label>
+                <input type="text" name="description" value="">
+            </div>
+
+            <button type="submit" name="button" value="Enter">Envoyer</button>
+
+        </form>
+
+    </body>
+</html>
